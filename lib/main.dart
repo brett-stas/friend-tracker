dade1e83aba7f7f1aa1e2f9473b499aada4a4c9e
Brett Stas <brettstas@Susans-MacBook-Air.local>
2026-03-01 13:54:24 +1100

    Add Firebase config: firebase_options.dart, google-services.json, minSdk 23

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:friend_tracker/config/theme.dart';
+import 'package:friend_tracker/firebase_options.dart';
 import 'package:friend_tracker/presentation/providers/auth_providers.dart';
 import 'package:friend_tracker/presentation/screens/home_shell.dart';
 import 'package:friend_tracker/presentation/screens/login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: FriendTrackerApp()));
 }
 
